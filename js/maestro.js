2b135d889a628931c8f7aa6e46b25bbbad8a0fc5
minefarmer <pgoldfarm@gmail.com>
2019-02-24 22:31:37 -0600

    shutting down Sunday in Win 10

---
+++ js/maestro.js
@@ -0,0 +1,15 @@
+let myDog = 'Maestro';
+let yourDog = myDog;
+myDog = 'Fido';
+
+console.log('My dog is: ' + yourDog);
+
+let myDog2 = {
+  name: 'Maestro'
+}
+let yourDog2 = myDog2;
+myDog2.name = 'Fido';
+
+console.log('My dog is: ' + yourDog2.name);
+
+// Ball 10 -- 59
